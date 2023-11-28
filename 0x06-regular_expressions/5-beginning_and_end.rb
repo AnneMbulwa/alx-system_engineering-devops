d4c500c5a00311c4b26fda06db8c2d7f64eeda3d
AnneMbulwa <annembulwa602@gmail.com>
2023-11-28 18:33:38 +0300

    regular expressions

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#matching from h to n and have a single character between
+puts ARGV[0].scan(/^h.n$/).join
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#match capitals only from a to z
+puts ARGV[0].scan(/[A-Z]*/).join
