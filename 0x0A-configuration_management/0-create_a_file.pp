f150803378e38b0ff97a3c41d6fe421823737ec3
AnneMbulwa <annembulwa602@gmail.com>
2024-01-19 19:16:59 +0300

    Configuration mgt

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # create a file in /tmp.
+
 file { '/tmp/school':
   ensure   => present,
-  mode     => 0744,
+  mode     => '0744',
   owner    => 'www-data',
   group    => 'www-data',
   content  => 'I love Puppet',
