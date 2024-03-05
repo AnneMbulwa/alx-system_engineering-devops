aabe157d16f834d985622d633a2ce236ea3a7978
AnneMbulwa <annembulwa602@gmail.com>
2024-03-06 01:12:58 +0300

    recursive

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""recursive function that queries the Reddit API and returns a
+list containing the titles of all hot articles for a given subreddit."""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+    resultx = response.json().get("data")
+    after = resultx.get("after")
+    count += resultx.get("dist")
+
+    for post in resultx.get("children"):
+        hot_list.append(post.get("data").get("tittle"))
+
+
+    if after != None:
+        return recurse(subreddit, hot_list, after)
+    
+    return hot_list
