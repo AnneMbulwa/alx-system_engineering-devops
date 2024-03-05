3114a7476a9addf63945deed464330d16f9525dd
AnneMbulwa <annembulwa602@gmail.com>
2024-03-06 00:45:16 +0300

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Function that queries Reddict API returns the number of subscribers
 that is nonactive users, total subscribers for given subscription"""
-
 import requests
 
 
@@ -11,6 +10,7 @@ def number_of_subscribers(subreddit):
     headers = {
             'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'
             }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         results = response.json().get("data")
