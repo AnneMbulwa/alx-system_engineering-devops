0be5a016fd0d1f2406139d9706488610a1a67e54
AnneMbulwa <annembulwa602@gmail.com>
2024-03-06 00:44:06 +0300

    hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """print top ten hot post"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
+    post = {
+            'limit': 10
+            }
+
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    result = response.json().get("data")
+    ([print(post.get("data").get("tittle"))for post in result.get("children")]
